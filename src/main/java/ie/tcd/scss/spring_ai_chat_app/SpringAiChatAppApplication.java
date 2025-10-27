bf9697b8ffbfc0fcdf0c17528774f5eaae34324a
huy <bhtran>
2025-10-27 23:30:54 +0000

    inital project

---
+++ src/main/java/ie/tcd/scss/spring_ai_chat_app/SpringAiChatAppApplication.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.spring_ai_chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/tcd/scss/spring_ai_chat_app/SpringAiChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.spring_ai_chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
